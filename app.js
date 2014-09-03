ff237270b19a4ab2723a6417957f26887ad8adfd
Jake Kalstad <JakeKalstad@gmail.com>
2014-09-02 23:52:13 -0600

    Some more scaffolding towards node implementation

--- app.js
+++ app.js
@@ -15,10 +15,8 @@ var category = require('./routes/category');
 var file = require('./routes/file');
 var filterdata = require('./routes/filterdata');
 var post= require('./routes/post');
-var session = require('./routes/session');
-
-var app = express();
-// view engine setup
+ 
+var app = express(); 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -35,13 +33,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/account', account);
-app.use('/state', state);  
-app.use('/location', location); 
+app.use('/state', state);
+app.use('/location', location);
 app.use('/category', category);
 app.use('/file', file);
 app.use('/filterdata', filterdata);  
-app.use('/post', post);     
-app.use('/session', session);
+app.use('/post', post);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/state.js
+++ routes/state.js
@@ -1,4 +1,6 @@
 var express = require('express');
 var router = express.Router();
- 
+router.post('/index', function(req, res) {
+  res.render('');
+});
 module.exports = router;
